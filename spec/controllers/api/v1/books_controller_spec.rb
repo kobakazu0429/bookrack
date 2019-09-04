71f4f759c1902261e9459066ffd99d964c02d681
kobakakzu0429 <kobakazu10@gmail.com>
2019-09-04 11:19:59 +0900

    add test

--- spec/controllers/api/v1/books_controller_spec.rb
+++ spec/controllers/api/v1/books_controller_spec.rb
@@ -10,9 +10,14 @@ describe Api::V1::BooksController, type: :controller do
       expect(response).to have_http_status(:success)
     end
 
-    it "correct returns data length" do
+    it "JSON body response expected length as seed_fu data" do
       json_response = JSON.parse(response.body)
       expect(json_response.length).to eq(4)
+    end
+
+    it "JSON body response contains expected recipe attributes" do
+      json_response = JSON.parse(response.body)
+      expect(json_response.first.keys).to match_array(["id", "title", "author", "isbn", "created_at", "updated_at"])
     end
   end
 end
